c721c6b6bd1686a9ce29295e19a3900239cf0c34
Jakob Valič <jakob.valic@student.fmf.uni-lj.si>
2017-05-19 13:30:36 +0200

    vaja s slovarji

--- Branje datotek/src/Branje.java
+++ Branje datotek/src/Branje.java
@@ -1,31 +1,43 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Branje {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println(deloZdatotekami("akcija.py"));
+		Map<String, Integer> besedisce = deloZdatotekami("HisaMarijePomocnice.txt");
+		System.out.println(besedisce.size());
+		Collection<String> zbirkaBesed = besedisce.keySet();
+		for (String kljuc : zbirkaBesed) {
+			Integer vrednost = besedisce.get(kljuc);
+			System.out.println(kljuc + ": " + vrednost);
+		}
 		
 	}
 
-	public static int deloZdatotekami(String imeVhod) throws IOException {
+	public static Map deloZdatotekami(String imeVhod) throws IOException {
 		BufferedReader vhod = new BufferedReader(new FileReader(imeVhod));
-		int stevec = 0;
+		Integer stevecVseh = 0;
+		Map<String, Integer> besedisce = new HashMap<String, Integer>(); // Mora biti objekt, torej int ne pride v poštev
 		while (vhod.ready()) {
 			String vrstica = vhod.readLine().trim();
 			if (vrstica.equals(""))
 				continue;
 			String[] besede = vrstica.split(" +");
-			stevec += besede.length; 
+			for (String beseda : besede) {
+				Integer stevec = 0;
+				if (besedisce.containsKey(beseda))
+					stevec += besedisce.get(beseda);
+				++stevec;
+				besedisce.put(beseda, stevec);
+			}
+			stevecVseh += besede.length; 
 		}
 		vhod.close();
-		return stevec;
+		return besedisce;
 	}
 	
-	public static int
 	
 }
